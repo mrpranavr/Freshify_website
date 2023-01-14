293f8a7b61a7f4f70b10cbfcf195eab9b94434a9
Pranav R <pranav81234@gmail.com>
2023-01-14 08:53:55 +0530

    added initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import React from "react";
+import "./index.css";
+
+import {
+    NavBar,
+    Hero,
+    Benefits,
+    Downloads,
+    Subscriptions,
+    CTA,
+    Contact,
+} from "./components/index";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import NavBar from "./Navbar";
+import Hero from "./Hero";
+import Benefits from "./Benefits";
+import Downloads from "./Downloads";
+import Subscriptions from "./Subscription";
+import CTA from "./CTA";
+import Contact from "./Contact";
+
+export { NavBar, Hero, Benefits, Downloads, Subscriptions, CTA, Contact };
